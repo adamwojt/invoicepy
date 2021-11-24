0c89b39002bda455ae84a2c6bb30670fda8110b4
Adam Wojtczak <adam1edinburgh@gmail.com>
2021-11-24 22:51:30 +0200

    tiny fix

--- tests/test_pyinvoice.py
+++ tests/test_pyinvoice.py
@@ -4,13 +4,13 @@
 
 from click.testing import CliRunner
 
-from pyinvoice import cli
+from pyinvoice.cli.main import cli
 
 
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.cli)
+    result = runner.invoke(cli)
     assert result.exit_code == 0
-    help_result = runner.invoke(cli.cli, ["--help"])
+    help_result = runner.invoke(cli, ["--help"])
     assert help_result.exit_code == 0
